2cc00f0cf7432f1aee9ea5fd155d1dabe8e0c061
Aly3107 <151246690+Aly3107@users.noreply.github.com>
2024-03-20 23:02:17 -0600

    terminado diccionario

--- Proyecto_Python/Categorias.py
+++ Proyecto_Python/Categorias.py
@@ -1,9 +1,24 @@
 categorias = {
-    "Animales_acuaticos" : ["Tiburon", "Delfin", "Tortuga", "Estrella de mar", "Pez globo", "Cachalote", "Pulpo", "Medusa", "Ballena", "Calamar", "León marino", "Foca", "Anguila eléctrica", "Sardina", "Atún", "Piraña"],
+    "Animales_bosque" : ["Águila", "Antílope", "Araña", "Armadillo", "Chimpance", "Escarabajo", "Guacamaya", "Hormiga", "Jabalí", "Jaguar", "Liebre", "Lince", "Luciérnaga", "Mariposa", "Mono araña", "Mono aullador", "Oso hormiguero", "Oso panda", "Oso pardo", "Puma", "Rana", "Sapo", "Serpiente", "Tucán", "Zarigüeya", "Zorro"],
 
-    "Animales_terrestres" : ["Camello", "Lobo", "Topo", "Liebre", "Pantera", "Gallina", "Gato", "Perro", "Tarántula", "Oveja", "Cerdo", "Iguana", "Búfalo", "Gusano", "Mapache", "Escorpión", "Elefante", "Venado", "Rinoceronte", "Avestruz", "Ganso", "Canguro"],
+    "Animales_desierto" : ["Adax", "Antílope", "Buitre", "Camello", "Cobra", "Coyote", "Fennec", "Culebra", "Jerbo", "Escorpión", "Correcaminos", "Salamandra tigre", "Tortuga Mojave", "Lobo gris", "Zorro"],
 
-    "Dinosaurios" : ["a"],
+    "Animales_selva" : ["Águila", "Anaconda", "Armadillo", "Boa", "Caimán", "Camaleón", "Chimpancé", "Elefante", "Gorila", "Guacamaya", "Hormiga", "Jaguar", "Lemur", "Leopardo", "Mono araña", "Murciélago", "Orangután", "Oso perezoso", "Pirañas", "Pitón", "Rana", "Tapir", "Tarántula", "Tigre", "Tucán"],
 
-    "Películas_animadas_Disney" : ["a"]
+    "Animales_montaña" : ["Águila dorada", "Coatí", "Cóndor", "Conejo de indias", "Gorila de montaña", "Lagarto de roca", "Leopardo", "Mono negro", "Panda rojo", "Tapir", "Vicuña", "Vibora"],
+
+    "Animales_oceano" : ["Almeja", "Araña de mar", "Ballena", "Calamar", "Crustáceo", "Camarón", "Cangejo", "Caracol marino", "Cocodrilo", "Delfín", "Erizo", "Estrella de mar", "Foca", "Langosta", "Manatí", "Medusa", "Mejillon", "Nutria", "Ostra", "Orca", "Pez bruja", "Pez espada", "Pingüino", "Pulpo", "Sanguijuela", "Tiburón blanco", "Tiburón toro", "Tortuga marina"],
+
+    "Dinosaurios_triasico" : ["Brachiosaurus", "Melanorosaurus", "Scelidosaurus"],
+
+    "Dinosaurios_jurasico" : ["Ichthyosaurus", "Ophtalmosaurus", "Plesiosaurus", "Compsognathus", "Allosaurus", "Megalosaurus", "Diplodocus", "Apatosaurus", "Seismosaurus", "Stegosaurus", "Kentrosaurus", "Scutellosaurus", "Drinker", "Camptosaurus", "Dimorphodon", "Caudipteryx", "Archaeopteryx"],
+
+    "Dinosaurios_cretacico" : ["Elasmosaurus", "Kronosaurus", "Mosasaurus", "Tyrannosaurus Rex", "Gigantosaurus", "Baryonyx", "Velociraptor", "Oviraptor", "Argentinosaurus", "Hadrosaurus", "Titanosaurus", "Ankylosaurus", "Edmontonia", "Minmi", "Triceratops", "Protoceratops", "Microceratops", "Leptoceratops", "Stygimoloch", "Styracosaurus", "Iguanodon", "Gallimimus", "Leaellynasaura", "Corythosaurus", "Malasaura", "Edmontosaurus", "Parasaurolophus", "Gryposaurus", "Pteranodon", "Hesperornis"],
+
+    "Peliculas_animadas_Disney" : ["101 Dalmatas", "Aladdín", "Alicia en el País de las Maravillas", "Atlantis El Imperio Perdido", "Bambi", "Bernardo y Bianca", "Big Hero 6", "Blancanieves y los 7 Enanitos", "Bolt", "Chicken Little", "Dinosaurio", "Dumbo", "El Caldero Mágico", "El Jorobado de Notre Dame", "El Libro de la Selva", "El Planeta del Tesoro", "El Rey León", "El Zorro y el Sabueso", "Encanto", "Enredados", "Frozen", "Hércules", "La Espada en la Piedra", "Las Locuras del Emperador", "La Bella Durmiente", "La Bella y la Bestia", "La Cenicienta", "La Dama y el Vagabundo", "La Familia del Futuro", "La Princesa y el Sapo", "La Sirenita", "Lilo & Stitch", "Los Aristogatos", "Moana", "Mulán", "Oliver y su Pandilla", "Policias y Ratones", "Peter Pan", "Pinocho", "Pocahontas", "Ralph el Demoledor", "Raya y el Último Dragón", "Robin Hood", "Tarzán", "Tierra de osos", "Vacas Vaqueras", "Winnie The Pooh", "Wish El Poder de los Deseos", "Zootopia"],
+    
+    "Peliculas_animadas_Pixar" : ["Bichos", "Buscando a Dory", "Buscando a Nemo", "Cars", "Coco", "Elementos", "Intensamente", "Lightyear", "Los Increíbles", "Luca", "Monsters Inc", "Ratatouille", "Red", "Soul", "Un Gran Dinosaurio", "Toy Story", "Up", "WALL-E", "Valiente"],
+
+    "Peliculas_animadas_Dreamworks" : ["Antz", "Bee Movie", "Cómo Entrenar a tu Dragón", "El Camino Hacia el Dorado", "El Espantatiburones", "El Gato con Botas", "El Origen de los Guardianes", "El Príncipe de Egipto", "Home", "Jefe en Pañales", "Kung Fu Panda", "Las Aventuras de Peabody y Sherman", "Las Aventuras del Capitán Calzoncillos", "Lo que el Agua se Llevó", "Los Croods", "Los Pingüinos de Madagascar", "Los Tipos Malos", "Madagascar", "Megamente", "Mounstros vs Aliens", "Pollitos en Fuga", "Ruby Aventuras de una Kraken Adolescente", "Shrek", "Simbad la Leyenda de los Siete Mares", "Spirit", "Trolls", "Turbo", "Un Amigo Abominable", "Vacinos Invasores", "Wallace y Gromit"]
+
 }
--- Proyecto_Python/funciones.py
+++ Proyecto_Python/funciones.py
@@ -7,15 +7,54 @@ def seleccionar_categoria(palabras):
     print("Seleccione una categoría: ")
     while True:
         print("""
-        1, Animales acuaticos
-        2, Animales terrestres
-        3, Dinosaurios
-        4, Películas animadas de Disney
+        1. Animales del bosque
+        2. Animales del desierto
+        3. Animales de la selva
+        4. Animales de la montaña
+        5. Animales del océano
+        6. Dinosaurios del périodo triásico
+        7. Dinosaurios del périodo jurásico
+        8. Dinosaurios del périodo cretácico
+        9. Peliculas animadas de Disney
+        10. Peliculas animadas de Pixar
+        11. Peliculas animadas de Dreamworks
         """)
-        categoria_seleccionada = input("Seleccione el número de acuerdo a la categoría: ")
+        categoria_seleccionada = input("Ingresa el número de acuerdo a la categoría: ")
         if categoria_seleccionada == "1":
-            palabra_secreta = random.choices(palabras["Animales_acuaticos"])
+            palabra_secreta = random.choices(palabras["Animales_bosque"])
             return palabra_secreta
+        elif categoria_seleccionada == "2":
+            palabra_secreta = random.choices(palabras["Animales_desierto"])
+            return palabra_secreta
+        elif categoria_seleccionada == "3":
+            palabra_secreta = random.choices(palabras["Animales_selva"])
+            return palabra_secreta
+        elif categoria_seleccionada == "4":
+            palabra_secreta = random.choices(palabras["Animales_montaña"])
+            return palabra_secreta
+        elif categoria_seleccionada == "5":
+            palabra_secreta = random.choices(palabras["Animales_oceano"])
+            return palabra_secreta
+        elif categoria_seleccionada == "6":
+            palabra_secreta = random.choices(palabras["Dinosaurios_triasico"])
+            return palabra_secreta
+        elif categoria_seleccionada == "7":
+            palabra_secreta = random.choices(palabras["Dinosaurios_jurasico"])
+            return palabra_secreta
+        elif categoria_seleccionada == "8":
+            palabra_secreta = random.choices(palabras["Dinosaurios_cretacico"])
+            return palabra_secreta
+        elif categoria_seleccionada == "9":
+            palabra_secreta = random.choices(palabras["Peliculas_animadas_Disney"])
+            return palabra_secreta
+        elif categoria_seleccionada == "10":
+            palabra_secreta = random.choices(palabras["Peliculas_animadas_Pixar"])
+            return palabra_secreta
+        elif categoria_seleccionada == "11":
+            palabra_secreta = random.choices(palabras["Peliculas_animadas_Dreamworks"])
+            return palabra_secreta
+        else: 
+            print("Número no valido, ingresa un número de acuerdo de su categoría")
 
 def mostrar_palabra(palabra, letras_adivinadas):
     visualizacion = ""
@@ -30,7 +69,35 @@ def letra_adivinada(palabra, letras_adivinadas):
     for letra in palabra:
         if letra not in letras_adivinadas:
             return False
+    return True
 
-palabra_secreta = seleccionar_categoria(palabras)
+def ahorcado():
+    palabra_secreta = seleccionar_categoria(palabras)
+    letras_adivinadas = []
+    intentos = 8
+
+print("¡Bienvenido al juego del Ahorcado!")
+print(mostrar_palabra(palabra, letras_adivinadas))
+
+while intentos >0:
+    intento = input("Adivina una letra: ").lower()
+    if intento in letras_adivinadas:
+        print("¡Bien, adivinaste una letra!")
+    elif intento in palabra_secreta:
+        letras_adivinadas.append(intento)
+        print("¡Vas bien!")
+    else:
+        intentos -= 1
+        print(f"""
+        ¡Letra incorrecta!, vuelve a intentarlo. 
+        Tienes {intentos} restantes""")
+
+    print(mostrar_palabra(palabra, letras_adivinadas))
+    if letra_adivinada(palabra, letras_adivinadas):
+        print("¡Felicidades!, haz adivinado la palabra.")
+        break
+    
+if intentos == 0:
+    print(f"¡Te has quedado sin intentos! La palabra secreta era: {palabra_secreta}")
 
 print(palabra_secreta)
